88a0d08c2c259aefb69828f7a78290a47f635f29
AhmedDiaa69 <diaaa625@gmail.com>
2025-02-01 18:31:52 +0200

    v1.0

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const start = document.getElementById("start");
 
 let rollCount = 0;
 let scoreCount = 0;
-let point = 0;
+let point;
 
 roll.addEventListener("click", () => {
-    start.style.display = "none";
     rollCount++;
     const dice1Value = Math.floor(Math.random() * 6) + 1;
     const dice2Value = Math.floor(Math.random() * 6) + 1;
@@ -20,22 +19,31 @@ roll.addEventListener("click", () => {
     score.textContent = `Scores: ${scoreCount}`;
 
     if (scoreCount === 7 && rollCount === 1) {
-        start.style.display = "block";
         start.textContent = "You won!";
     } else if ((scoreCount === 2 || scoreCount === 3 || scoreCount === 12) && rollCount === 1) {
-        start.style.display = "block";
-        start.textContent = "You lost!";
-        
+        start.textContent = "You lost! the house wins";
+        rollCount = 0;
+        point;
     } else if (rollCount != 7 && rollCount === 1) {
-
+        point = scoreCount;
+        start.textContent = "The reach point is: " + point;
+    } else if (scoreCount === point) {
+        start.textContent = "You won!";
+        rollCount = 0;
+        point;
+    } else if (scoreCount === 7 && rollCount != 1) {
+        start.textContent = "You lost! the house wins";
+        rollCount = 0;
+        point;
     }
 });
 
 reset.addEventListener("click", () => {
-    start.style.display = "block";
+    start.textContent = "Roll the dice";
     dice1.src = "no1.png";
     dice2.src = "no1.png";
     rollCount = 0;
     scoreCount = 0;
+    point;
     score.textContent = `Scores: 0`;
 });
